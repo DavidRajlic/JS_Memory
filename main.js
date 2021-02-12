4c83b412824d5144dd6340c3809cd27506d61083
DavidR <rajlic.david@gmail.com>
2021-02-12 21:30:44 +0100

    add files

---
+++ main.js
@@ -0,0 +1,7 @@
+const btn = document.querySelector('.button');
+const btn1 = document.querySelector('button');
+//btn.style.display = 'none'; ko boš dobo par
+
+btn.addEventListener('click', function (e) {
+btn.classList.add('.borderadd');
+});
