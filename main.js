8c032fe4bc077550aa0c98fcf0336c4acdbabf03
DavidR <rajlic.david@gmail.com>
2021-02-14 22:11:51 +0100

    position

--- main.js
+++ main.js
@@ -1,7 +1,1 @@
-const btn = document.querySelector('.button');
-const btn1 = document.querySelector('button');
-//btn.style.display = 'none'; ko boš dobo par
 
-btn.addEventListener('click', function (e) {
-btn.classList.add('.borderadd');
-});
