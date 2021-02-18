712b6a58af9ba8796f9dcdf51bb94b80baf7fb8c
DavidR <rajlic.david@gmail.com>
2021-02-18 20:35:27 +0100

    image shows when click on card

--- main.js
+++ main.js
@@ -1,1 +1,16 @@
+const cards = Array.from(document.querySelectorAll('.card'));
+const img = Array.from(document.querySelectorAll('img'));
 
+for (let i=0; i<img.length; i++){
+  img[i].style.visibility = "hidden";
+};
+
+for (let i=0; i<cards.length; i++){
+  cards[i].addEventListener('click', function myFunction (e) {
+  cards[i].classList.add('red');
+  img[i].style.visibility =  "visible";
+
+});
+};
+
+ 
