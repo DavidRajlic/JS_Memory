d6f9a5a61b1795af1ae4ad9c18aaf707cdd5bdf6
DavidR <rajlic.david@gmail.com>
2021-02-19 17:35:07 +0100

    cards on random positions

--- main.js
+++ main.js
@@ -1,11 +1,16 @@
 const cards = Array.from(document.querySelectorAll('.card'));
 const img = Array.from(document.querySelectorAll('img'));
-
+// cards on random positions
+  const  position = document.querySelector('.position');
+  for ( let  i = position.children.length; i >= 0; i--) {
+      position.appendChild(position.children[Math.random() * i | 0]);
+  }
+  //hide image visibility
 for (let i=0; i<img.length; i++){
   img[i].style.visibility = "hidden";
 };
-
-for (let i=0; i<cards.length; i++){
+// when click on card show image and border
+for (let i=0; i<img.length; i++){
   cards[i].addEventListener('click', function myFunction (e) {
   cards[i].classList.add('red');
   img[i].style.visibility =  "visible";
@@ -13,4 +18,6 @@ for (let i=0; i<cards.length; i++){
 });
 };
 
- 
+
+
+
