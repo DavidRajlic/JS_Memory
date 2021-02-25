a61876aaa9606062bc8f63d8aaece2535e9648e9
DavidR <rajlic.david@gmail.com>
2021-02-25 21:19:33 +0100

    Memory finished

--- main.js
+++ main.js
@@ -25,42 +25,51 @@ let cardIndex = {
 
 for (let i=0; i < cards.length; i++) {
   cards[i].addEventListener('click', function (e) {
-    // if card is already clicked, do nothing
-    // check if first card is already clicked
-    // if 2 cards are clicked and not a pair, disable clicks until setTimeout is done
-
-    
-    // if first card is not clicked save first index
     if ( cardIndex.first < 0) {
      cardIndex.first = i;
     } else {
       cardIndex.second = i;
     }
-  
 
     cardsClicked++;
-  
+
+    if( cardsClicked === 1){
+     cards[cardIndex.first].style.pointerEvents = 'none';
+    } 
+
     if (cardsClicked <= 2) {
       cards[i].classList.add('red');
       img[i].style.visibility = 'visible';
     }
     
-    if (cardsClicked === 2) {
+    if (cardsClicked === 2) { 
+      for (let j=0; j < cards.length; j++){
+       cards[j].style.pointerEvents = 'none';
+      };
+      
+      cards[cardIndex.first].style.pointerEvents = 'none';
+      cards[cardIndex.second].style.pointerEvents = 'none';
+      
       setTimeout(function time () {
         cards[cardIndex.first].classList.remove('red');
-        img[cardIndex.first].style.visibility =  "hidden";
         cards[cardIndex.second].classList.remove('red');
-        img[cardIndex.second].style.visibility =  "hidden";
 
-        if (cardIndex.second >= 0) {
-          if (img[cardIndex.first].className === img[cardIndex.second].className) {
-            cards[cardIndex.first].style.visibility =  'hidden';
-            cards[cardIndex.second].style.visibility =  'hidden';
-          }
+        img[cardIndex.first].style.visibility = 'hidden';
+        img[cardIndex.second].style.visibility =  'hidden';
+      
+        if (img[cardIndex.first].className === img[cardIndex.second].className) {
+          cards[cardIndex.first].style.visibility = 'hidden';
+          cards[cardIndex.second].style.visibility = 'hidden';
         }
-
+        for (let j=0; j < cards.length; j++){
+          cards[j].style.pointerEvents = 'auto';
+         };
+         
+         cards[cardIndex.second].style.pointerEvents = 'auto';
+        
         cardIndex.first = -1;
         cardIndex.second = -1;
+        
         cardsClicked=0;
       }, 700);
     }
